142b73952383bea76a66a330ad10ee63d7ce0a6e
Maksym Serhiyevich Kushnirenko <100068913+Keernex@users.noreply.github.com>
2023-05-08 21:09:47 +0300

    no done

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	cout << "Kraj Lukomor'a dub zelenyj, " << endl;
+	Beep(1000, 1000);
+	cout << "I zolotyj Lancyuh na nim: " << endl;
+	Beep(1000, 1000);
+	cout << "Shodnya, shonochi kit uchenyj " << endl;
+	Beep(1000, 1000);
+	cout << "Na Lancyuhu kruzhl'aje tim" << endl;
+	Beep(1000, 1000);
+	return 0;
+}
