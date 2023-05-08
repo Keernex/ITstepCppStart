025cedb9fcc845a9ec62bbbc195f215e492d47dd
Maksym Serhiyevich Kushnirenko <100068913+Keernex@users.noreply.github.com>
2023-05-08 22:29:25 +0300

    done ex-3

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_All, "ru");
+	cout << (char)1 << endl;
+	return 0;
+}
+//cout << "╔════════════════════════════════════════════╗" << endl;
+//cout << endl;
+//cout << "║\t\t" << "Pory Roku" << "\t\t\t║" << endl;
