b28a5f513a3036bda44fe0771465d469e811cd09
Maksym Serhiyevich Kushnirenko <100068913+Keernex@users.noreply.github.com>
2023-05-08 22:55:38 +0300

    done ex-3

--- Source.cpp
+++ Source.cpp
@@ -4,13 +4,15 @@ using std::cout;
 
 int main()
 {
-	setlocale(LC_ALL, "ru");
-	for (int i = 0; i <= 255; i++)
-	{
-		cout << (char)i << endl;
-	}
-	return 0;
+    setlocale(LC_ALL, "ru");
+    cout << "╔════════════════════════════════════════════╗" << endl;
+    cout << endl;
+    cout << "║\t\t" << "Pory Roku" << "\t\t      ║" << endl;
+    cout << endl;
+    cout << "╠═════════╦═══════════╦═══════════╦══════════╣" << endl;
+    cout << endl;
+    cout << "║   Zyma  ║   Vesna   ║    Lito    ║    Osin  ║" << endl;
+    cout << endl;
+    cout << "╚═════════╩═══════════╩════════════╩══════════╝" << endl;
+    return 0;
 }
-//cout << "╔════════════════════════════════════════════╗" << endl;
-//cout << endl;
-//cout << "║\t\t" << "Pory Roku" << "\t\t\t║" << endl;
