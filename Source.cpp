6b09dcd5e635bc3cdd72742bf6b854b2280ecdce
Maksym Serhiyevich Kushnirenko <100068913+Keernex@users.noreply.github.com>
2023-05-08 23:44:16 +0300

    done ex-3

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using std::endl;
+using std::cout;
+using std::cin;
+int main()
+{
+    double q;
+    double w;
+    double e;
+    cin >> q;
+    cin >> w;
+    cin >> e;
+
+    cout << q * w + (e * w * 2) / 2;
+}
