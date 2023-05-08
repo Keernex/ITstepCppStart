06dd2052756c15077f43925c5fd79c9c99121a5d
Maksym Serhiyevich Kushnirenko <100068913+Keernex@users.noreply.github.com>
2023-05-08 21:01:51 +0300

    done ex-1

---
+++ Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::endl;
+using std::cout;
+
+
+int main()
+{
+    cout << "Name       :" << " \"The war and the peace\"" << endl;
+    cout << "Avtor      :" << " L.N. Tolstoj" << endl;
+    cout << "Izdatelstvo:" << " Piter" << endl;
+    cout << "Pages      :" << " 500." << endl;
+    return 0;
+}
